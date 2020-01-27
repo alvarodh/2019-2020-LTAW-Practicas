81f038c5ba6bb3acbd2d21c82293ff052e0f55c1
Lvaro Del Hoyo Arias <alvarodh@f-l3208-pc08.aulas.etsit.urjc.es>
2020-01-27 12:03:01 +0100

    modificado server para que busque el recurso y devuelva el mensaje adecuado

--- P1/server.js
+++ P1/server.js
@@ -4,7 +4,10 @@ const port = 8080;
 // Modulo http
 const http = require('http');
 
-console.log("Arrancando servidor...")
+// Modulo para la obtencion de los recursos que pide el cliente
+const fs = require('fs');
+
+console.log('Arrancando servidor...')
 
 // Funcion para atender peticiones
 // req → solicitud
@@ -12,12 +15,42 @@ console.log("Arrancando servidor...")
 function peticion(req, res) {
 
   // Peticion recibida
-  console.log("Peticion recibida!")
+  console.log('Peticion recibida!')
 
-  // Mensaje de respuesta
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  // Buscamos en la url el recurso que se pide
+  var url = req.url.slice(1).split('.')[0];
 
+  // Miramos si tenemos ese recurso
+  switch (url) {
+    case '':
+      // Si no hay nada redirigimos a la pagina inicial
+      fs.readFile('layout/index.html', 'utf-8', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        console.log('index.html require');
+        return res.end();
+      });
+      break;
+    case 'recurso1':
+    case 'recurso2':
+    case 'recurso3':
+      // Si es alguno de los recursos conocidos, se le redirige
+      fs.readFile('layout/' + url + '.html', 'utf-8', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        console.log(url + ' require');
+        return res.end();
+      });
+      break;
+    default:
+      // Si no se conoce el recurso, se redirige a la página de error
+      fs.readFile('layout/error.html', 'utf-8', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        console.log('error.html require');
+        return res.end();
+      });
+  }
 }
 
 // Inicializar el servidor
@@ -27,5 +60,5 @@ const server = http.createServer(peticion)
 // Configurar el servidor para escuchar en el puerto establecido
 server.listen(port);
 
-console.log("Super-Servidor LISTO!")
-console.log("Escuchando en puerto: " + port)
+console.log('Super-Servidor LISTO!')
+console.log('Escuchando en puerto: ' + port)
