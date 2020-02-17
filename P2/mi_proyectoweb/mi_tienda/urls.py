7cea67a85cff3ac3a3ad58fdef140c0366260763
Lvaro Del Hoyo Arias <alvarodh@f-l3208-pc08.aulas.etsit.urjc.es>
2020-02-17 12:53:43 +0100

    avanzada tienda, colocados tres articulos y asignadas urls para webs dinamicas

--- P2/mi_proyectoweb/mi_tienda/urls.py
+++ P2/mi_proyectoweb/mi_tienda/urls.py
@@ -9,9 +9,7 @@ from . import views
 urlpatterns = [
     # -- Vista pricipal (índice)
     path('', views.index, name='index'),
-    path('test1/', views.test1, name='test1'),
-    path('test2/', views.test2, name='test2'),
-    path('test3/', views.test3, name='test3'),
-    path('test4/', views.test4, name='test4'),
-    path('test5/', views.test5, name='test5'),
+    path('producto1/', views.producto1, name='producto1'),
+    path('producto2/', views.producto2, name='producto2'),
+    path('producto3/', views.producto3, name='producto3'),
 ]
