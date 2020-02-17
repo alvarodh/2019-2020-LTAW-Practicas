b5a7365659070d5bb11cdeb6082311149bf5703c
Lvaro Del Hoyo Arias <alvarodh@f-l3208-pc08.aulas.etsit.urjc.es>
2020-02-17 11:36:43 +0100

    ¡primera (cutre)web creada con éxito!

---
+++ P2/mi_proyectoweb/mi_tienda/views.py
@@ -0,0 +1,8 @@
+# -- Fichero mi_tienda/views.py
+from django.http import HttpResponse
+
+# -- Vista principal de mi tienda
+# -- El nombre de la vista puede ser cualquiera. Nosotros lo hemos
+# -- llamado index, pero se podría haber llamado pepito
+def index(request):
+    return HttpResponse("Hola! esta es la página principal de Mi tienda!")
